b90cca51c0a856d28ba212e5725ca5e1eac08f4d
thierry <legros.thl@gmail.com>
2026-09-20 17:48:47 +0200

    exo sur le D02 et la transformation de data

---
+++ M02-Data-Analysis/D02_data_transformation/tls_test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as ps
+
+np.random.seed(42)
+
+# random_number_1 = np.random.random(5)
+# print("first sequence: ",random_number_1)
+# random_number_1 = np.random.random(5)
+# print("second sequence: ",random_number_1)
+# random_number_1 = np.random.random(5)
+# print("third sequence: ",random_number_1)
+
+random_2d_array = np.random.random((3, 4))
+print("2D array:\n ", random_2d_array)
